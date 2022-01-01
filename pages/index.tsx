2055bbfbecc2a371df00b19ce6d9a19a9a263263
devarea <devarea@pop-os.localdomain>
2022-01-01 17:42:49 +0530

    Fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import { Box, Button, Image, useColorMode } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { FaBeer } from "react-icons/fa";
 import { useEffect } from "react";
+import styles from "../styles/Home.module.css";
+import HomeConfig from "../config/home";
 const Home: NextPage = () => {
-  return <Box className="[grid-area:restofstuff] w-full"></Box>;
+  return (
+    <Box className="[grid-area:restofstuff] w-full grid place-items-center ">
+      <h1 className="text-4xl md:text-6xl">
+        {"Hi, I am "}
+        <span className={`text-blue-700 ${styles.underlineanimation}`}>
+          {HomeConfig.name}
+        </span>
+      </h1>
+    </Box>
+  );
 };
 
 export default Home;
