7eb33f12c8ef869a3ebbfa1a2dc7c33453b6948a
devarea <devarea@pop-os.localdomain>
2022-01-01 20:31:39 +0530

    Fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,27 +8,30 @@ import HomeConfig from "../config/home";
 
 const Home: NextPage = () => {
   return (
-      <div className="flex flex-col items-center justify-center w-2/3 space-y-3">
-        <h1 className="text-4xl md:text-5xl font-bold">
-          {"Hi, I am "}
-          <span className={`text-blue-500 py-2 break-words`}>{HomeConfig.name}</span>
-        </h1>
-        <h3 className="sm:text-base md:text-lg lg:text-xl text-gray-400">
-          {HomeConfig.description}
-        </h3>
-        <div className="flex space-x-2 items-center justify-center">
-          {HomeConfig.social_links.map(({ name, link, icon }, i) => (
-            <IconButton
-              variant="outline"
-              colorScheme="blue"
-              aria-label={name}
-              fontSize="20px"
-              icon={icon}
-              isRound={true}
-            />
-          ))}
-        </div>
+    <div className="flex flex-col items-center justify-center w-2/3 space-y-3">
+      <h1 className="text-4xl md:text-5xl font-bold">
+        {"Hi, I am "}
+        <span className={`text-blue-500 py-2 break-words`}>
+          {HomeConfig.name}
+        </span>
+      </h1>
+      <h3 className="sm:text-base md:text-lg lg:text-xl text-gray-400">
+        {HomeConfig.description}
+      </h3>
+      <div className="flex space-x-2 items-center justify-center">
+        {HomeConfig.social_links.map(({ name, link, icon }, i) => (
+          <IconButton
+            key={i}
+            variant="outline"
+            colorScheme="blue"
+            aria-label={name}
+            fontSize="20px"
+            icon={icon}
+            isRound={true}
+          />
+        ))}
       </div>
+    </div>
   );
 };
 
